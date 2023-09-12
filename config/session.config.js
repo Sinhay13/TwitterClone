fa5e7a52790c4bc0b1866d8d328f69031bf0307a
Yahnis <yahnis.allegret@yahoo.com>
2023-09-12 13:15:06 +0200

    start session

---
+++ config/session.config.js
@@ -0,0 +1,2 @@
+const { app } = require('../app'); // on recupere notre fichier app a la racine du projet 
+const session = require('express-session'); // on invoque le midleware de expresse pour gérer les sessions
