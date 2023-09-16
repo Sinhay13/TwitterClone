058702c8c53a5abd9d733b92b532cb2a9461997e
Yahnis <yahnis.allegret@yahoo.com>
2023-09-16 12:32:28 +0200

    connection system done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,30 @@
-const passport = require('passport');
+const passport = require('passport'); // we call passport for the authentication 
 
-exports.signinForm = (req, res, next) => {
-  res.end();
+exports.signinForm = (req, res, next) => { // acces to the form for the connection 
+  res.render('auth/auth-form', { errors: null }); 
 }
 
-exports.signin = (req, res, next) => {
-    res.end();
+exports.signin = (req, res, next) => { // to connect
+  passport.authenticate('local', (err, user, info) => { // call passport in local 
+    if (err) {
+      next(err);// errors for middleware 
+    } else if (!user) { // if not good user 
+      res.render('auth/auth-form', { errors: [ info.message ] }); // show error message 
+    } else {
+      req.login(user, (err) => { // succeed redirect to home page
+        if (err) { next(err) } else {
+          res.redirect('/tweets');
+        }
+      })
+    }
+  })(req, res, next);
 }
 
-exports.signout = (req, res, next) => {
-    res.end();
+exports.signout = (req, res, next) => { // log out and redirect to sign in 
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/auth/signin/form'); 
+  });
 }
-
-
-// next step
