e2f66691b59352da778e824a144c6954ffae43cf
Yahnis allegret <yahnis.allegret@yahoo.com>
2023-10-25 16:55:56 +0900

    project stope

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,55 +1,39 @@
-// Import the 'createUser' function from the 'users.queries' module
-const { createUser } = require('../queries/users.queries');
-const path = require('path')// to deal with image address
-const multer = require('multer');// to call multer
-const upload= multer({ storage: multer.diskStorage({ // to handle images
-  destination: (req, file, cb) => {
-    cb(null, path.join( __dirname, '../public/images/avatars'))// localization 
+const { createUser } = require('../queries/users.queries'); // Importing the createUser function from the users.queries module
+const path = require('path'); // Importing the path module
+const multer = require('multer'); // Importing the multer module for handling file uploads
+const upload = multer({ storage: multer.diskStorage({ // Creating a multer instance with disk storage options
+  destination: (req, file, cb) => { // Setting the destination directory for uploaded files
+    cb(null, path.join( __dirname, '../public/images/avatars'))
   },
-  filename: (req, file, cb)=>{
-    cb(null, `${ Date.now() }-${ file.originalname }` );// to give a name 
+  filename: (req, file, cb) => { // Setting the filename for uploaded files
+    cb(null, `${ Date.now() }-${ file.originalname }`);
   }
-})});
+}) })
 
-// Render the signup form
-exports.signupForm = (req, res, next) => { 
-  // Render the 'users/user-form' view with the specified data
-  res.render('users/user-form', {
-    errors: null,
-    isAuthenticated: req.isAuthenticated(),
-    currentUser: req.user
-  });
+exports.signupForm = (req, res, next) => { // Exporting a function for rendering the user signup form
+  res.render('users/user-form', { errors: null, isAuthenticated: req.isAuthenticated(), currentUser: req.user });
 }
 
-// Handle user signup
-exports.signup = async (req, res, next) => {
-  const body = req.body;
+exports.signup = async (req, res, next) => { // Exporting an async function for handling user signup
+  const body = req.body; // Extracting the request body
   try {
-    // Create a new user using the 'createUser' function
-    const user = await createUser(body);
-    // Redirect to the homepage after successful signup
-    res.redirect('/');
-  } catch (e) {
-    // If there's an error during signup, render the signup form with an error message
-    res.render('users/user-form', {
-      errors: [e.message],
-      isAuthenticated: req.isAuthenticated(),
-      currentUser: req.user
-    });
+    const user = await createUser(body); // Creating a new user with the createUser function
+    res.redirect('/'); // Redirecting to the home page
+  } catch(e) {
+    res.render('users/user-form', { errors: [ e.message ], isAuthenticated: req.isAuthenticated(), currentUser: req.user }); // Rendering the user signup form with an error message if there was an error creating the user
   }
 }
 
-// Middleware to upload user avatar
-exports.uploadImage = [
-  upload.single('avatar'), // Call the upload function with the 'avatar' field
-  async (req, res, next) => { 
-    try{
-      const user = req.user; // Get the current user
-      user.avatar = `/images/avatars/${ req.file.filename} `; // Set the user's avatar to the uploaded file
-      await user.save(); // Save the user
-      res.redirect('/'); // Redirect to the homepage after successful upload
-    }catch(e){
-      next(e); // Handle errors
+exports.uploadImage = [ // Exporting an array of middleware functions for handling user avatar uploads
+  upload.single('avatar'), // Using multer to handle the file upload
+  async (req, res, next) => { // Async function for updating the user's avatar
+    try {
+      const user = req.user; // Getting the current user from the request object
+      user.avatar = `/images/avatars/${ req.file.filename }`; // Setting the user's avatar path to the uploaded file
+      await user.save(); // Saving the user to the database
+      res.redirect('/'); // Redirecting to the home page
+    } catch(e) {
+      next(e); // Passing any errors to the error handling middleware
     }
   }
 ]
